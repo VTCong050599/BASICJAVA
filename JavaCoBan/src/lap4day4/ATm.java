f42e2259bd4cb87ee06a6837349303e8ee3a3ca3
Võ Thành Công <54182697+VTCong050599@users.noreply.github.com>
2019-08-20 20:31:00 +0700

    update atm

--- JavaCoBan/src/lap4day4/ATm.java
+++ JavaCoBan/src/lap4day4/ATm.java
@@ -1,9 +1,9 @@
 package lap4day4;
 
 public interface ATm{
-	boolean withdraw(int x,double y);
-	boolean deposit(int x,double y);
-	double queryBalance(int x);
-	boolean login(String x,String y);
+	boolean withdraw(int sotk,double sotienrut);
+	boolean deposit(int sotk,double sotiengui);
+	double queryBalance(int sotk);
+	boolean login(String sotaikhoan,String matkhau);
 	boolean logout(String x);
 }
